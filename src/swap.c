155ad4ee684877573aef78c75e7ea8bb2a436700
Jean SPEZIA <jspezia@e3r9p11.42.fr>
2015-02-23 16:34:38 +0100

    p

--- src/swap.c
+++ src/swap.c
@@ -6,7 +6,7 @@
 /*   By: jspezia <jspezia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/23 14:40:09 by jspezia           #+#    #+#             */
-/*   Updated: 2015/02/23 16:23:39 by jspezia          ###   ########.fr       */
+/*   Updated: 2015/02/23 16:33:44 by jspezia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 #include "dlist.h"
 #include "push_swap.h"
 
-void		sa(t_list_ps *dlist)
+void		sa(t_list_ps *list)
 {
 	t_dlist_node	*swap;
 
 	ft_putendl("sa");
-	swap = dlist->a->first->next;
+	swap = list->a->first->next;
 	if (swap->next)
-		swap->next->prev = dlist->a->first;
-	dlist->a->first->next = swap->next;
-	dlist->a->first->prev = swap;
+		swap->next->prev = list->a->first;
+	list->a->first->next = swap->next;
+	list->a->first->prev = swap;
 	swap->next = swap->prev;
 	swap->prev = NULL;
-	dlist->a->first = swap;
+	list->a->first = swap;
 }
