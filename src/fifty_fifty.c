e21ae2bc4d1455fb3c8f4bcb6a69b507f829d433
Jean SPEZIA <jspezia@e3r9p11.42.fr>
2015-02-24 19:32:28 +0100

    50/50

--- src/fifty_fifty.c
+++ src/fifty_fifty.c
@@ -3,9 +3,11 @@
 void			fill_stack_b(t_op ops[OPS_LEN], t_ps *ps)
 {
 	int		count;
+	int		count_a;
 
 	count = 0;
-	while (count < COUNT(ps->stack_a) / 2)
+	count_a = COUNT(ps->stack_a);
+	while (count < count_a / 2)
 	{
 		call_op(PB, ops, ps);
 		count++;
@@ -27,24 +29,42 @@ void			fifty_fifty(t_op ops[OPS_LEN], t_ps *ps)
 	{
 		node_a = FIRST(ps->stack_a);
 		node_b = FIRST(ps->stack_b);
-		if (NEXT_VAL(node_a) == min_a && NEXT_VAL(node_b) == min_b)
-		 	call_op(RR, ops, ps);
-		else if (NEXT_VAL(node_a) == min_a)
-			call_op(RA, ops, ps);
-		else if (NEXT_VAL(node_b) == min_b)
-		 	call_op(RB, ops, ps);
-		//**********
-		else if (CURR_VAL(node_a) > NEXT_VAL(node_a) && CURR_VAL(node_b) > NEXT_VAL(node_b))
-			call_op(SS, ops, ps);
-		else if (CURR_VAL(node_a) > NEXT_VAL(node_a))
-			call_op(SA, ops, ps);
+		if (NEXT_VAL(node_b) == min_b)
+		{
+			if (NEXT_VAL(node_a) == min_a)
+				call_op(RR, ops, ps);
+			else
+				call_op(RB, ops, ps);
+		}
 		else if (CURR_VAL(node_b) > NEXT_VAL(node_b))
-			call_op(SB, ops, ps);
-		//**********
-		else if (!is_stack_sorted(ps->stack_a))
-			call_op(RR, ops, ps);
+		{
+			if (CURR_VAL(node_a) > NEXT_VAL(node_a))
+				call_op(SS, ops, ps);
+			else
+				call_op(SB, ops, ps);
+		}
 		else
-			call_op(RRB, ops, ps);
+		{
+			call_op(RB, ops, ps);
+		}
+		// node_a = FIRST(ps->stack_a);
+		// node_b = FIRST(ps->stack_b);
+		// if (NEXT_VAL(node_a) == min_a && NEXT_VAL(node_b) == min_b)
+		//  	call_op(RR, ops, ps);
+		// else if (NEXT_VAL(node_a) == min_a)
+		// 	call_op(RA, ops, ps);
+		// else if (NEXT_VAL(node_b) == min_b)
+		//  	call_op(RB, ops, ps);
+		// else if (CURR_VAL(node_a) > NEXT_VAL(node_a) && CURR_VAL(node_b) > NEXT_VAL(node_b))
+		// 	call_op(SS, ops, ps);
+		// else if (CURR_VAL(node_a) > NEXT_VAL(node_a))
+		// 	call_op(SA, ops, ps);
+		// else if (CURR_VAL(node_b) > NEXT_VAL(node_b))
+		// 	call_op(SB, ops, ps);
+		// else if (!is_stack_sorted(ps->stack_a))
+		// 	call_op(RR, ops, ps);
+		// else
+		// 	call_op(RRB, ops, ps);
 	}
 	ft_putendl("stack_b sorted !");
 	while (!is_stack_sorted(ps->stack_a))
