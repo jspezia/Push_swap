9fd1ca2c3be76679570e4c0771c042ef31c92252
Jean SPEZIA <jspezia@e3r13p10.42.fr>
2015-02-26 18:09:32 +0100

    controle

--- src/algo_waves.c
+++ src/algo_waves.c
@@ -38,46 +38,71 @@ void		waves_sort(t_op ops[OPS_LEN], t_ps *ps)
 	t_dlist_node	*node_a;
 	t_dlist_node	*node_b;
 
-	int				min;
 	int				i;
 	int				waves_a;
 	int				waves_b;
 
 	i = 0;
-	min = find_min(ps->stack_a);
 	waves_b = 0;
 	while (!is_resolved(ps) && ps->total_ops < MAX_OPS)
 	{
-		while (i != 0)
-		{
-			up(ops, ps, min);
-			i++;
+		// while (i != 5)
+		// {
+		// 	up(ops, ps, ps->range_min);
+		// 	i++;
 			waves_a = compt_waves(ps->stack_a);
-			ft_printf("%d waves,  %d ps->total_ops\n", waves_a, ps->total_ops);
-		}
+		// 	ft_printf("%d waves,  %d ps->total_ops\n", waves_a, ps->total_ops);
+		// }
+
 		while (waves_a > waves_b)
 		{
 			OP(PB);
 			waves_a = compt_waves(ps->stack_a);
 			waves_b = compt_waves_reverse(ps->stack_b);
 		}
+		// node_b = FIRST(ps->stack_b);
+		// node_a = LAST(ps->stack_a);
+		// while (node_b)
+		// {
+		// 	if (CURR_VAL(node_b) > CURR_VAL(node_a))
+		// 		OP(PA);
+		// 	else if (CURR_VAL(node_a) < PREV_VAL(node_a))
+		// 	{
+		// 		OP(RRA);
+		// 		OP(PA);
+		// 	}
+		// 	else
+		// 		OP(RRA);
+		// 	node_a = LAST(ps->stack_a);
+		// 	node_b = FIRST(ps->stack_b);
+		// }
+
 
 		node_b = FIRST(ps->stack_b);
 		node_a = LAST(ps->stack_a);
 		while (node_b)
 		{
 			if (CURR_VAL(node_b) > CURR_VAL(node_a))
-				OP(PA);
-			else if (CURR_VAL(node_a) < PREV_VAL(node_a))
 			{
-				OP(RRA);
+				ft_putendl(node_b > node_a);
 				OP(PA);
 			}
-			else
+			else if (CURR_VAL(node_b) < CURR_VAL(node_a))
+			{
+				ft_putendl(node_b < node_a);
 				OP(RRA);
+			}
+			else if (CURR_VAL(node_a) > CURR_VAL(FIRST(ps->stack_a)))
+			{
+				ft_putendl(node_b > FIRST(node_a));
+				OP(PA);
+			}
 			node_a = LAST(ps->stack_a);
 			node_b = FIRST(ps->stack_b);
 		}
+
+
+
 		waves_a = compt_waves(ps->stack_a);
 		waves_b = compt_waves_reverse(ps->stack_b);
 		ft_printf("%d waves_a,  %d waves_b\n", waves_a, waves_b);
